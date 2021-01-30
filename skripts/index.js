6063d31fce61dda762dd463f83ae51a1187fdc20
Tikhon <morgunov.t@gmail.com>
2021-01-30 16:10:53 +0300

    fix: переписать попап, избавившись от абсолютного позиционирования

--- skripts/index.js
+++ skripts/index.js
@@ -1,16 +1,16 @@
-let overlay = document.querySelector('.overlay'); // Объявил оверлей с попапом
+let overlay = document.querySelector('.popup'); // Объявил оверлей с попапом
 let openButton = document.querySelector('.profile__edit-button'); // Объявил кнопку открытия попапа
-let closeButton = document.querySelector('.overlay__close-button'); // Объявил кнопку закрытия попапа
+let closeButton = document.querySelector('.popup__close-button'); // Объявил кнопку закрытия попапа
 
-let formElement = document.querySelector('.overlay__popup');
-let nameInput = document.querySelector('.overlay__input_item_name');
-let captureInput = document.querySelector('.overlay__input_item_capture'); // Объявил элементы для формы и полей ввода формы.
+let formElement = document.querySelector('.popup__contaner');
+let nameInput = document.querySelector('.popup__input_item_name');
+let captureInput = document.querySelector('.popup__input_item_capture'); // Объявил элементы для формы и полей ввода формы.
 
 let profileName = document.querySelector('.profile__name');
 let profileCapture = document.querySelector('.profile__capture'); // Объявил элементы, куда нужно вставлять введеный текст из формы
 
 function openOverlay() { //"overlay" в функции - это объявленная ранее переменная в DOM | При методе classList точку перед именем класса ставить не нужно
-  overlay.classList.remove('overlay_display_none'); // Метод classList.remove удаляет модификатор display_none, раскрывая оверлей с попапом
+  overlay.classList.add('popup_opened'); // Метод classList.add добавляет модификатор, раскрывая оверлей с попапом
   nameInput.value = profileName.textContent;
   captureInput.value = profileCapture.textContent; // Эти две строки передают исходный текст с открывшуюся форму, однако с кучей пробелов слева и справа
 }
@@ -18,7 +18,7 @@ function openOverlay() { //"overlay" в функции - это объявлен
 openButton.addEventListener('click', openOverlay); // Повесил addEventListener на переменную openButton c аргументами click и openOverlay | openOverlay здесь - ранее объявленная функция
 
 function closeOverlay() {
-  overlay.classList.add('overlay_display_none'); // Метод classList.add добавляет модификатор display_none, скрывая оверлей с попапом
+  overlay.classList.remove('popup_opened'); 
 }
 
 closeButton.addEventListener('click', closeOverlay); // Аналогично предыдущему "слушателю"
@@ -29,7 +29,7 @@ function formSubmitHandler(evt) {
   profileName.textContent = nameInput.value;
   profileCapture.textContent = captureInput.value; // При помощи textContent перезаписывается на страницу то что сейчас введено в input
 
-  overlay.classList.add('overlay_display_none');
+  overlay.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
