1de0a10666c39ff4f6efe34d71990251e4073259
Tikhon <morgunov.t@gmail.com>
2021-02-01 14:43:40 +0300

    refactor: Применить комментарии ревьюера
    Собрал все обработчики в одном месте в конце кода
    Избавился от дублирования кода
    Структурировал комментарии к коду

--- skripts/index.js
+++ skripts/index.js
@@ -1,3 +1,5 @@
+// Объявленные переменные и DOM элементы
+
 let overlay = document.querySelector('.popup'); // Объявил оверлей с попапом
 let openButton = document.querySelector('.profile__edit-button'); // Объявил кнопку открытия попапа
 let closeButton = document.querySelector('.popup__close-button'); // Объявил кнопку закрытия попапа
@@ -9,19 +11,21 @@ let captureInput = document.querySelector('.popup__input_item_capture'); // Об
 let profileName = document.querySelector('.profile__name');
 let profileCapture = document.querySelector('.profile__capture'); // Объявил элементы, куда нужно вставлять введеный текст из формы
 
+// Функция открытия оверлея с попапом
+
 function openOverlay() { //"overlay" в функции - это объявленная ранее переменная в DOM | При методе classList точку перед именем класса ставить не нужно
   overlay.classList.add('popup_opened'); // Метод classList.add добавляет модификатор, раскрывая оверлей с попапом
   nameInput.value = profileName.textContent;
   captureInput.value = profileCapture.textContent; // Эти две строки передают исходный текст с открывшуюся форму, однако с кучей пробелов слева и справа
 }
 
-openButton.addEventListener('click', openOverlay); // Повесил addEventListener на переменную openButton c аргументами click и openOverlay | openOverlay здесь - ранее объявленная функция
+// Функция закрытия оверлея с попапом
 
 function closeOverlay() {
   overlay.classList.remove('popup_opened'); 
 }
 
-closeButton.addEventListener('click', closeOverlay); // Аналогично предыдущему "слушателю"
+// Функция сохранения изменений данных профиля
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -29,7 +33,11 @@ function formSubmitHandler(evt) {
   profileName.textContent = nameInput.value;
   profileCapture.textContent = captureInput.value; // При помощи textContent перезаписывается на страницу то что сейчас введено в input
 
-  overlay.classList.remove('popup_opened');
-}
+  function closeOverlay() {} // Использую уже существующую функцию внутри новой
+  }
 
+// Обработчики
+
+openButton.addEventListener('click', openOverlay); // Повесил addEventListener на переменную openButton c аргументами click и openOverlay | openOverlay здесь - ранее объявленная функция
+closeButton.addEventListener('click', closeOverlay); // Аналогично предыдущему "слушателю"
 formElement.addEventListener('submit', formSubmitHandler);
