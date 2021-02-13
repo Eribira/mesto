b9223aa34ef51304e7cee114eeb488ca0688af81
Tikhon <morgunov.t@gmail.com>
2021-02-13 15:16:48 +0300

    Вынес массив с объектами в отдельный js

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,3 @@
-
-const initialCards = [ // Массив с объектами для карточек
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: 'Горный пейзаж'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: 'Вид на зимнее озеро'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alt: 'Фото многоэтажного района'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: 'Заснеженная горная вершина'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alt: 'Фото железной дороги в лесу'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: 'Фото скалы у озера Байкал'
-  }
-];
-
 // Код сгруппирован по принципу:
 // Dom элементы и const для фичи
 // Function для нее же
